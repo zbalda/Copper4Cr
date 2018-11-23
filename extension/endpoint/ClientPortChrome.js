9a792a583e3189f4b9b77fc0dbb87e55fd88db23
Zachary Balda <zacharybalda23@gmail.com>
2018-11-23 15:46:42 -0500

    configure chrome extension

--- extension/endpoint/ClientPortChrome.js
+++ extension/endpoint/ClientPortChrome.js
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2016, Institute for Pervasive Computing, ETH Zurich.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -13,7 +13,7 @@
  * 3. Neither the name of the Institute nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -25,7 +25,7 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- * 
+ *
  * This file is part of the Copper (Cu) CoAP user-agent.
  ******************************************************************************/
 
@@ -36,10 +36,10 @@ Copper.ClientPort = function(){
 };
 
 Copper.ClientPort.connect = function(clientId, finalDisconnectHandler, callback){
-	
+
 	// UPDATE with value from chrome://extensions/
-	let appId = "olkcgjkdhadideeemhnfbihnbmbpfjnd";
-	
+	let appId = "pfcjcjialabnknmimiohooegcjohllkk";
+
 	let port = new Copper.Port(chrome.runtime.connect(appId), clientId);
 
 	let connectedCallback = function(){
